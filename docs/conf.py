2ce1919d2b984c4da41b8f4b0dc2137bdae8e0b9
Dernerd <webmaster@n3rds.work>
2024-02-09 23:47:12 +0100

    Delete index.md

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Padma Sphinx Theme'
-copyright = u'Padma Unlimited'
+copyright = u'PSOURCE'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
